25b1da9c2d971e7683a37c6d62defc4c64f2968a
Peter Bolfa <xbolfa@fi.muni.cz>
2020-04-09 19:20:51 +0200

    Removed unused methods

--- csharp/Tennis/TennisGame2.cs
+++ csharp/Tennis/TennisGame2.cs
@@ -13,7 +13,6 @@ namespace Tennis
 
         public string GetScore()
         {
-            var score = "";
             if (IsTie())
             {
                 return IsDeuce() ? "Deuce" : points[p1point] + "-All";
@@ -22,81 +21,35 @@ namespace Tennis
             {
                 return points[p1point] + "-" + points[p2point];
             }
-            if (Player1HasAdvantage() || Player2HasAdvantage())
-            {
-                score = "Advantage player";
-                score += Player1HasAdvantage() ? "1" : "2";
-            }
             if (Player1Won() || Player2Won())
             {
-                score = "Win for player";
-                score += Player1Won() ? "1" : "2";
+                return "Win for player" + (Player1Won() ? "1" : "2");
             }
-            return score;
-        }
-
-        private bool IsDeuce()
-        {
-            return IsTie() && p1point > 2;
-        }
-
-        private bool Player1HasAdvantage()
-        {
-            return p1point > p2point && p2point >= 3;
-        }
-
-        private bool Player2HasAdvantage()
-        {
-            return p2point > p1point && p1point >= 3;
-        }
-
-        private bool Player2Won()
-        {
-            return p2point >= 4 && p1point >= 0 && (p2point - p1point) >= 2;
-        }
-
-        private bool Player1Won()
-        {
-            return p1point >= 4 && p2point >= 0 && (p1point - p2point) >= 2;
-        }
-
-        private bool IsTie()
-        {
-            return p1point == p2point;
-        }
-
-        public void SetP1Score(int number)
-        {
-            for (int i = 0; i < number; i++)
+            if (Player1HasAdvantage() || Player2HasAdvantage())
             {
-                P1Score();
+                return "Advantage player" + (Player1HasAdvantage() ? "1" : "2");
             }
+            return "";
         }
 
-        public void SetP2Score(int number)
-        {
-            for (var i = 0; i < number; i++)
-            {
-                P2Score();
-            }
-        }
+        private bool IsDeuce() => IsTie() && p1point > 2;
 
-        private void P1Score()
-        {
-            p1point++;
-        }
+        private bool Player1HasAdvantage() => p1point > p2point && p2point >= 3;
 
-        private void P2Score()
-        {
-            p2point++;
-        }
+        private bool Player2HasAdvantage() => p2point > p1point && p1point >= 3;
+
+        private bool Player2Won() => p2point >= 4 && p1point >= 0 && (p2point - p1point) >= 2;
+
+        private bool Player1Won() => p1point >= 4 && p2point >= 0 && (p1point - p2point) >= 2;
+
+        private bool IsTie() => p1point == p2point;
 
         public void WonPoint(string player)
         {
             if (player == "player1")
-                P1Score();
+                p1point++;
             else
-                P2Score();
+                p2point++;
         }
 
     }
